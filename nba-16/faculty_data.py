47340e91c0b1f07faa7fd9f67814eabd7158600a
Pallavi2000 <hmpsharma@gmail.com>
2020-07-29 20:32:42 +0530

    nba-16 retriving the faculty data part-2

--- nba-16/faculty_data.py
+++ nba-16/faculty_data.py
@@ -9,7 +9,7 @@ users = mydb['dhi_user']
 term_detail = mydb['dhi_term_detail']
 lesson_plan = mydb['dhi_lesson_plan']
 generic_attainment_configuration = mydb['dhi_generic_attainment_config']
-events = mydb['dhi_generic_attainment_data']
+generic_attainment_data = mydb['dhi_generic_attainment_data']
 
 def get_course_code():
     courses = lesson_plan.aggregate([
@@ -38,3 +38,36 @@ def get_course_code():
         course_codes.append(course)
     
     return course_codes
+
+def get_cos_of_courses():
+    cos = generic_attainment_data.aggregate([
+            {
+                "$unwind":"$faculties"
+            },
+            {
+                "$unwind":"$courseOutcomeDetailsForAttainment"
+            },
+            {
+                "$match":{"academicYear":"2018-19",
+                    "faculties.facultyGivenId":"172",
+                    "termNumber":"3",
+                    "courseDetails.courseCode":"17CS36"
+                }
+            },
+            {
+                "$project":{"coNumber":"$courseOutcomeDetailsForAttainment.coNumber",
+                    "total_attainment":"$courseOutcomeDetailsForAttainment.totalAttainment",
+                    "courseCode":"$courseDetails.courseCode",
+                    "section":1,
+                    "_id":0
+                }
+            }
+        ])
+    
+    co_details = []
+
+    for co in cos:
+        co_details.append(co)
+    
+    return co_details
+
