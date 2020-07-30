d53d74a0346c0ccab90ca87fbde7dba8ae308b36
AnushaShetty <anushashetty669@gmail.com>
2020-07-30 17:01:41 +0530

    selection screen api

--- app.py
+++ app.py
@@ -7,6 +7,43 @@ from mongoFlask import MongoJSON_Encoder
 app = Flask(__name__)
 app.json_encoder = MongoJSON_Encoder
 
+@app.route('/principal/academicyear')
+def get_academicYear_principal():
+    year = st_17.get_academicYear_principal()
+    return jsonify({"year":year})
+
+@app.route('/principal/department_term')
+def get_dept_term_principal(academicYear = '2018-19'):
+    data = st_17.get_dept_term_principal(academicYear)
+    return jsonify({"dept_and_term":data})
+
+@app.route('/hod/dept')
+def dep_hod(employeeGivenId = '583'):
+    dept_hod = st_17.get_dept_hod(employeeGivenId)
+    return dep_hod
+
+@app.route('/hod/academicyear')
+def get_academicYear_hod(dept_hod = 'CS' ,employeeGivenId = '583'):
+    hod_academicYear = st_17.get_academicYear_hod(dept_hod) 
+    return jsonify({"hod_academicYear":hod_academicYear})
+
+@app.route('/hod/terms')
+def get_term_hod(academicYear = "2018-19",dept = 'CS'):
+    data = st_17.get_terms_hod(academicYear,dept)
+    return jsonify( { "hod_terms" : data} )
+ 
+ @app.route('/faculty/academicyear')
+
+@app.route('/faculty/academicyear')
+def get_academicYear_faculty(facultyGivenId = '492'):
+    faculty_academicYear = st_17.get_academicYear_faculty(facultyGivenId)
+    return jsonify({"faculty_academicYear":faculty_academicYear})
+
+@app.route("/faculty/terms")
+def get_terms_faculty(facultyGivenId = '492',academicYear = '2018-19'):
+    faculty_terms_data = st_17.get_terms_faculty(facultyGivenId,academicYear)
+    return jsonify({"faculty_terms":faculty_terms_data})
+
 @app.route("/terms")
 def get_terms():
     terms_data = st_17.get_terms_details()
--- statement17.py
+++ statement17.py
@@ -8,6 +8,68 @@ generic_attainment_data = mydatabase['dhi_generic_attainment_data']
 lesson_plan = mydatabase['dhi_lesson_plan']
 term_detail = mydatabase['dhi_term_detail']
 
+def get_academicYear_principal():
+    querry = lesson_plan.aggregate([ {'$group' : { '_id': 'null',"academicYear" : {'$addToSet' : "$academicYear"} } }  ])
+    return [q for q in querry]
+    
+def get_dept_term_principal(academicYear):
+    querry = lesson_plan.aggregate([
+            {'$match' : { "academicYear" : academicYear }},
+            {'$project' : { "departments.deptId" : 1, "departments.termNumber":1 }},
+            {'$unwind' : "$departments"},
+            {'$group' : {'_id': 'null', 
+                "depts" : {'$addToSet' : "$departments.deptId"},
+                "terms" : {'$addToSet' : "$departments.termNumber"}
+                }}
+        ])
+    return [q for q in querry]
+
+def get_dept_hod(employeeGivenId):
+    dept_hod = dhi_user.find({'employeeGivenId': employeeGivenId },{'deptId':1})
+    return [dept for dept in  dept_hod]
+
+def get_academicYear_hod(dept):
+    querry = lesson_plan.aggregate([
+    { '$match' : {"faculties.facultyDeptId":'CS'}},
+    {'$group': { '_id' : 'null',
+        "all_academic_year" : {'$addToSet': "$academicYear"}
+        }},
+    {'$project' : {'_id' : 0, "all_academic_year":1}}
+    ])
+    return [q for q in querry]
+
+def get_terms_hod(academicYear,dept):
+    querry = lesson_plan.aggregate([
+        {'$match' : {"departments.deptId" : dept, "academicYear":academicYear}},
+        {'$project' : {'_id':0,"departments.termNumber":1}},
+        {'$unwind' : "$departments"},
+        {'$group' : {'_id' : 'null', "hod_terms":{'$addToSet' : "$departments.termNumber"}}},
+        {'$project' : {'_id': 0}}
+        ])
+    return [q for q in querry]
+
+def get_academicYear_faculty(facultyGivenId):
+    querry = lesson_plan.aggregate([
+    {'$match' : {"faculties.facultyGivenId":'492'}},
+    {'$group': { '_id' : 'null',
+        "all_academic_year" : {'$addToSet' : "$academicYear"}
+        }},
+    {'$project' : {'_id' : 0, "all_academic_year":1}}
+    ])
+    return [q for q in querry]
+
+def  get_terms_faculty(facultyGivenId, academicYear):
+    querry = lesson_plan.aggregate([
+        { '$match':{
+            'faculties.facultyGivenId':facultyGivenId,'academicYear': academicYear 
+        }},
+        {'$unwind' : '$departments'},
+        {'$group' : {'_id' : 'null', 'terms' : {'$addToSet' : '$departments.termNumber'}}},
+        {'$project': {'_id':0,'terms':1}}
+        ]) 
+    return [q for q in querry]
+
+
 def get_terms_details(): 
     results =  mydatabase.term_detail.find({})
     data = []
